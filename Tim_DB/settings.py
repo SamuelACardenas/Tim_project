c40ea44df6a415b24d4fb3ab476ecf4f8e08c706
SamuelACardenas <134441054+SamuelACardenas@users.noreply.github.com>
2025-11-11 15:53:52 -0600

    add conf. settings to Time_zone

--- Tim_DB/settings.py
+++ Tim_DB/settings.py
@@ -110,6 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
+CELERY_ENABLE_UTC = False
+DJANGO_CELERY_BEAT_TZ_AWARE = False
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'America/Mexico_City'
