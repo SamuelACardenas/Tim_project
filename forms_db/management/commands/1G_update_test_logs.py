5c853eaa53bddfd3cc8b7d336448d1b8af4e5b36
foxGit23 <gitfoxconn@gmail.com>
2025-12-13 01:06:46 -0600

    Update Script 1G_update_test_logs

--- forms_db/management/commands/1G_update_test_logs.py
+++ forms_db/management/commands/1G_update_test_logs.py
@@ -29,14 +29,14 @@ class Command(BaseCommand):
         """
         
         estaciones_dict = [
-            #{"ip": "10.12.199.150", "nombre": "RUNIN 01", "usuario": "User"},
-            #{"ip": "10.12.199.155", "nombre": "RUNIN 02", "usuario": "user"},
-            #{"ip": "10.12.199.160", "nombre": "RUNIN 03", "usuario": "User"},
+            {"ip": "10.12.199.150", "nombre": "RUNIN 01", "usuario": "User"},
+            {"ip": "10.12.199.155", "nombre": "RUNIN 02", "usuario": "user"},
+            {"ip": "10.12.199.160", "nombre": "RUNIN 03", "usuario": "User"},
             {"ip": "10.12.199.165", "nombre": "RUNIN-04", "usuario": "user"},
-            #{"ip": "10.12.199.140", "nombre": "BFT 01", "usuario": "User.DESKTOP-T3G3HJ2"},
-            #{"ip": "10.12.199.145", "nombre": "BFT 02", "usuario": "User"},
-            #{"ip": "10.12.199.170", "nombre": "FCA 01", "usuario": "user"},
-            #{"ip": "10.12.199.175", "nombre": "FCA 02", "usuario": "user"},
+            {"ip": "10.12.199.140", "nombre": "BFT 01", "usuario": "User.DESKTOP-T3G3HJ2"},
+            {"ip": "10.12.199.145", "nombre": "BFT 02", "usuario": "User"},
+            {"ip": "10.12.199.170", "nombre": "FCA 01", "usuario": "user"},
+            {"ip": "10.12.199.175", "nombre": "FCA 02", "usuario": "user"},
         ]
         
         for estacion in estaciones_dict:
